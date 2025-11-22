f1463f1aaf5867aa001343bc40aaa9eecd276191
palina-mrk <khudziakova@tut.by>
2025-11-22 17:22:26 +0300

    try to add css

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./js/script.js",
+  resolve: {
+      modulesDirectories: ['node_modules']
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
@@ -18,26 +21,11 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
-    loaders: [
-      {
-          test: /\.js/,
-          loader: 'babel',
-          exclude: /(node_modules|bower_components)/
-      },
-      {
-          test: /\.css$/,
-          loader: 'style!css'
-      }
-    ]
-  },
-  resolve: {
-      modulesDirectories: ['node_modules']
   },
   plugins: [
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: "index.html",
-    }),
-    new ExtractTextPlugin('bundle.css'),
+    })
   ],
 };
