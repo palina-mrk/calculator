2277a17cc074f232c4a4ab9ffe4dc224ce229c0f
palina-mrk <khudziakova@tut.by>
2025-11-23 14:53:27 +0300

    add keyboard events

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const colorInputs = {
 }
 
 const valueInput = document.querySelector('#num-value');
+valueInput.readOnly = true;
 
 //body
 const bodyEl = document.querySelector('.calculator');
@@ -68,8 +69,6 @@ Object.keys(colorInputs).forEach((inputEl) => {
     return;
   })
 })
-
-
 
 btnsStandard.forEach((btnEl, ind) => {
   btnEl.addEventListener("click", () => {
@@ -155,7 +154,7 @@ btnsTop.percent.addEventListener('click', () => {
 
 // function to save operation type
 Object.keys(btnsRight).forEach((btnEl) => {
-  btnsRight[btnEl].addEventListener("click", (event) => {
+  btnsRight[btnEl].addEventListener("click", () => {
     if(btnEl !== 'evaluate'){
       lastOp = btnEl;
       valueSaved = valueVisible;
@@ -188,6 +187,138 @@ Object.keys(btnsRight).forEach((btnEl) => {
     valueInput.value = valueVisible;
     return;
   })
+})
+
+document.addEventListener('keypress', (event) => {
+  let result;
+  console.log(event.code);
+
+  switch (event.key) {
+
+    case 'Delete': 
+      valueVisible = '0';
+      isDot = false;
+      valueSaved = '0';
+      valueInput.value = valueVisible;
+      return;
+
+    case 'Enter':
+    case '=': 
+      switch (lastOp){
+        case 'divide':
+          result = Number (valueVisible) ? Number(valueSaved) / Number (valueVisible) : 'error';     
+          break; 
+        case 'multiply':
+          result = Number(valueSaved) * Number (valueVisible);     
+          break; 
+        case 'add':
+          result = Number(valueSaved) + Number (valueVisible);     
+          break; 
+        case 'subtract':
+          result = Number(valueSaved) - Number (valueVisible);     
+          break;
+        default: 
+          return; 
+      }
+
+      valueSaved = valueVisible; 
+      valueVisible = getResult(result);
+      isDot = valueVisible.includes('.');
+      valueInput.value = valueVisible;
+      return;
+
+    case '/': 
+      lastOp = 'divide';
+      valueSaved = valueVisible;
+      valueVisible = '0';
+      isDot = false;
+      return;
+
+    case '*': 
+      lastOp = 'multiply';
+      valueSaved = valueVisible;
+      valueVisible = '0';
+      isDot = false;
+      return;
+
+    case '+': 
+      lastOp = 'add';
+      valueSaved = valueVisible;
+      valueVisible = '0';
+      isDot = false;
+      return;
+
+    case '-': 
+      lastOp = 'subtract';
+      valueSaved = valueVisible;
+      valueVisible = '0';
+      isDot = false;
+      return;
+
+
+    case '%': 
+      switch (lastOp){
+        case 'divide':
+          result = Number (valueVisible) ? Number(valueSaved) * 100 / Number (valueVisible) : 'error';     
+          break; 
+        case 'multiply':
+          result = Number(valueSaved) * Number (valueVisible) / 100;     
+          break; 
+        case 'add':
+          result = Number(valueSaved) * ( 1 + Number (valueVisible) / 100 );     
+          break; 
+        case 'subtract':
+          result = Number(valueSaved) * (1 - Number (valueVisible) / 100);     
+          break;
+        default: 
+          return; 
+      }
+      valueSaved = valueVisible; 
+      valueVisible = getResult(result);
+      isDot = valueVisible.includes('.');
+      valueInput.value = valueVisible;
+      return;
+
+    case '.':
+    case ',': 
+      if(valueVisible.length >= maxLength)
+        return;
+      if(valueVisible == 'error')
+        return;
+
+      if(!isDot){
+        isDot = true;
+        valueVisible += '.';
+        valueInput.value = valueVisible;
+      } 
+      return;
+      
+    case '0': 
+    case '1': 
+    case '2': 
+    case '3': 
+    case '4': 
+    case '5': 
+    case '6': 
+    case '7': 
+    case '8': 
+    case '9':
+      if(valueVisible.length >= maxLength)
+        return;
+      if(valueVisible == 'error')
+        return;
+
+      // btn = index value
+      if(valueVisible == '0')
+        valueVisible = event.key; 
+      else 
+        valueVisible += event.key;
+      
+      valueInput.value = valueVisible;
+      return;
+
+  }
+
 })
 
 // для сборки через webpack
